ed7e6223e5a5026f97a9988ed2652af3915ec855
gmunkhuchral-debug <gmunkhuchral@gmail.com>
2026-05-17 11:48:07 +0800

    о

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
  * Bump CACHE_VERSION whenever index.html or assets change so phones pick up new code.
  */
 
-const CACHE_VERSION = 'chimun-tasks-v16-bank-account-2026-05-17';
+const CACHE_VERSION = 'chimun-tasks-v17-role-based-2026-05-17';
 const SHELL_FILES = [
   './',
   './index.html',

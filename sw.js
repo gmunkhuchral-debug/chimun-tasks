b668f72facb4ea4ce5da460904938513221266d5
gmunkhuchral-debug <ceo@nomaadcamp.com>
2026-05-19 08:03:50 +0800

    a

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
  * Bump CACHE_VERSION whenever index.html or assets change so phones pick up new code.
  */
 
-const CACHE_VERSION = 'chimun-tasks-v31-pin-fetch-retry-2026-05-18';
+const CACHE_VERSION = 'chimun-tasks-v32-mobile-clean-2026-05-18';
 const SHELL_FILES = [
   './',
   './index.html',
